e82e0f46c00c5951263a1164b20e5832aa4303be
= <=>
2023-11-07 14:44:06 +0800

    Add google maps and firestore

--- src/App.js
+++ src/App.js
@@ -1,25 +1,79 @@
-import logo from './logo.svg';
+import { useState, useCallback, useEffect } from 'react';
 import './App.css';
+import { GoogleMap, useJsApiLoader, Marker } from '@react-google-maps/api';
+import { addData, deleteQuest } from './utils/firebase.js';
+
+const containerStyle = {
+  width: '100vw',
+  height: '100vh'
+};
+
+const center = {
+  lat: 34.0292077,
+  lng: -118.420331
+};
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [markers, setMarkers] = useState([]);
+  const GOOGLE_MAPS_API_KEY = process.env.REACT_APP_GOOGLE_API_KEY ?? "";
+  const [map, setMap] = useState(null);
+
+  useEffect(() => {
+    return () => {
+      // delete quest when user leaves the page
+      deleteQuest();
+    }
+  }, [])
+  const _onClick = async (e) => {
+    if (e.latLng) {
+      const lat = e.latLng.lat();
+      const lng = e.latLng.lng();
+      const markersCopy = [...markers];
+      if (!markersCopy.some((m) => m.lat === lat && m.lng === lng)) {
+        markersCopy.push({lat, lng});
+        await addData(lat, lng, markersCopy.length).then(() => setMarkers(markersCopy))
+      }
+    }
+  }
+
+  const { isLoaded } = useJsApiLoader({
+    id: 'google-map-script',
+    googleMapsApiKey: GOOGLE_MAPS_API_KEY
+  })
+
+  const onLoad = useCallback((map) => {
+    // This is just an example of getting and using the map instance!!! don't just blindly copy!
+    const bounds = new window.google.maps.LatLngBounds(center);
+    map.fitBounds(bounds);
+    setMap(map)
+  }, [])
+
+  const onUnmount = useCallback(async (map) => {
+    setMap(null)
+    await deleteQuest();
+  }, [])
+
+  const Markers = markers.length && markers.map((value, index) => 
+    <Marker
+      key={index}
+      position={{ lat: value.lat, lng: value.lng}}
+      label={`${index + 1}`}
+    />
+  )
+
+  return isLoaded ? (
+      <GoogleMap
+        mapContainerStyle={containerStyle}
+        center={center}
+        zoom={12}
+        onLoad={onLoad}
+        onUnmount={onUnmount}
+        onClick={_onClick}
+        key={GOOGLE_MAPS_API_KEY}
+      >
+        {Markers}
+      </GoogleMap>
+  ) : <></>
 }
 
 export default App;
